e6ae2338ddee44c76fb1483c190834ed75f543cd
Ihor <igormochalov1881@gmail.com>
2023-04-30 20:16:05 +0300

    Input installer

---
+++ Assets/_Code/Installers/InputInstaller.cs
@@ -0,0 +1,17 @@
+﻿using Zenject;
+
+public class InputInstaller : MonoInstaller
+{
+    private InputActions _input;
+
+    public override void InstallBindings()
+    {
+        _input = new InputActions();
+        _input.Enable();
+
+        Container
+            .Bind<InputActions>()
+            .FromInstance(_input)
+            .AsSingle();
+    }
+}
