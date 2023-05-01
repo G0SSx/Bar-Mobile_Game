7afd6f3c6f0f171f6fe2b58bd1646151f0ebdd2c
Ihor <igormochalov1881@gmail.com>
2023-05-01 23:51:00 +0300

    trash counter script

---
+++ Assets/_Code/Counters/TrashCounter.cs
@@ -0,0 +1,13 @@
+﻿public class TrashCounter : BaseCounter
+{
+    public override KitchenObject Interact(KitchenObject playersObject)
+    {
+        if (playersObject != null)
+        {
+            playersObject.HasBeenTaken?.Invoke();
+            Destroy(playersObject.transform.gameObject);
+        }
+
+        return null;
+    }
+}
