58f81afb79a43dd9c874fe5910b01db0a4d8d281
Ihor <igormochalov1881@gmail.com>
2023-05-01 23:52:09 +0300

    Kitchen object + object type enum

---
+++ Assets/_Code/KitchenObjects/KitchenObject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+public class KitchenObject : MonoBehaviour
+{
+    public Action HasBeenTaken;
+
+    public void SetParent(Transform parent)
+    {
+        transform.position = parent.transform.position;
+        transform.rotation = parent.transform.rotation;
+        transform.SetParent(parent);
+    }
+}
---
+++ Assets/_Code/KitchenObjects/KitchenObjectType.cs
@@ -0,0 +1,7 @@
+﻿public enum KitchenObjectType
+{
+    Cheese = 0,
+    Tomato = 1,
+    Cabbage = 2, 
+    Meat = 3,
+}
