4b1d5f3b6653762137c7946a4a20d08a3646ffd7
Ihor <igormochalov1881@gmail.com>
2023-05-01 23:51:41 +0300

    Kitchen objects factory + factories installer

---
+++ Assets/_Code/Installers/FactoriesInstaller.cs
@@ -0,0 +1,18 @@
+﻿using Zenject;
+using UnityEngine;
+
+public class FactoriesInstaller : MonoInstaller
+{
+    [SerializeField] private KitchenObject _meatPrefab;
+    [SerializeField] private KitchenObject _cheesePrefab;
+    [SerializeField] private KitchenObject _cabbagePrefab;
+    [SerializeField] private KitchenObject _tomatoPrefab;
+
+    public override void InstallBindings()
+    {
+        Container
+            .Bind<KitchenObjectsFactory>()
+            .FromInstance(new KitchenObjectsFactory(_cabbagePrefab, _meatPrefab, _tomatoPrefab, _cheesePrefab))
+            .AsSingle();
+    }
+}
---
+++ Assets/_Code/KitchenObjects/KitchenObjectsFactory.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class KitchenObjectsFactory
+{
+    private KitchenObject _cabbagePrefab;
+    private KitchenObject _meatPrefab;
+    private KitchenObject _tomatoPrefab;
+    private KitchenObject _cheesePrefab;
+
+    public KitchenObjectsFactory(KitchenObject cabbagePrefab, KitchenObject meatPrefab, 
+        KitchenObject tomatoPrefab, KitchenObject cheesePrefab)
+    {
+        _cabbagePrefab = cabbagePrefab;
+        _meatPrefab = meatPrefab;
+        _tomatoPrefab = tomatoPrefab;
+        _cheesePrefab = cheesePrefab;
+    }
+
+    public KitchenObject CreateKitchenObject(KitchenObjectType type, Transform objectParent)
+    {
+        KitchenObject kitchenObject = null;
+
+        switch (type)
+        {
+            case KitchenObjectType.Tomato:
+                kitchenObject = Object.Instantiate(_tomatoPrefab);
+                break;
+            case KitchenObjectType.Meat:
+                kitchenObject = Object.Instantiate(_meatPrefab);
+                break;
+            case KitchenObjectType.Cheese:
+                kitchenObject = Object.Instantiate(_cheesePrefab);
+                break;
+            case KitchenObjectType.Cabbage:
+                kitchenObject = Object.Instantiate(_cabbagePrefab);
+                break;
+        }
+
+        if (kitchenObject != null)
+        {
+            kitchenObject.transform.SetParent(objectParent);
+            kitchenObject.transform.position = objectParent.position;
+            kitchenObject.transform.rotation = objectParent.rotation;
+        }
+
+        return kitchenObject;
+    }
+}
