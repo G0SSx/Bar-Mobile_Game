2a3b5f6768c23d7dd438e5e54091b628035cd704
Ihor <igormochalov1881@gmail.com>
2023-05-01 23:50:19 +0300

    Conteiner counter script

---
+++ Assets/_Code/Counters/ConteinerCounter.cs
@@ -0,0 +1,26 @@
+﻿using Zenject;
+using UnityEngine;
+
+public class ConteinerCounter : BaseCounter
+{
+    [SerializeField] private KitchenObjectType _objectType;
+    [SerializeField] private Transform _objectParent;
+    [SerializeField] private Animator _animator;
+
+    private const string OpeningAnimationTriggerName = "OpenClose";
+
+    private KitchenObjectsFactory _factory;
+
+    [Inject]
+    private void Construct(KitchenObjectsFactory factory) => 
+        _factory = factory;
+
+    public override KitchenObject Interact(KitchenObject objectInPlayersHands)
+    {
+        if (objectInPlayersHands != null)
+            return null;
+
+        _animator.SetTrigger(OpeningAnimationTriggerName);
+        return _factory.CreateKitchenObject(_objectType, _objectParent); ;
+    }
+}
