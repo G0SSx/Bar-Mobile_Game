188575247434778213afe393a68f64b7836f8dac
Ihor <igormochalov1881@gmail.com>
2023-05-05 00:59:08 +0300

    ContainmentCounter class added

---
+++ Assets/_Code/Counters/Logic/ContainmentCounter.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System;
+
+public abstract class ContainmentCounter : BaseCounter
+{
+    [SerializeField] private Transform _kitchenObjectParent;
+
+    protected KitchenObject kitchenObject { get; private set; }
+
+    protected KitchenObject ReturnKitchenObject()
+    {
+        KitchenObject objectToReturn = kitchenObject;
+        kitchenObject = null;
+        return objectToReturn;
+    }
+
+    protected KitchenObject TakeKitchenObject(KitchenObject kitchenObject)
+    {
+        if (kitchenObject == null)
+            throw new ArgumentNullException();
+
+        kitchenObject.HasBeenTaken?.Invoke();
+        this.kitchenObject = kitchenObject;
+        this.kitchenObject.SetParent(_kitchenObjectParent);
+        return null;
+    }
+}
