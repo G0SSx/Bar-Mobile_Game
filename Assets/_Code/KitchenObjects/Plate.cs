c99fd923f97ecc79a33b075bc1c7eb37e9a1b4a3
Ihor <igormochalov1881@gmail.com>
2023-05-05 01:02:24 +0300

    Plate class added

---
+++ Assets/_Code/KitchenObjects/Plate.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Plate : KitchenObject
+{
+    [SerializeField] private Transform _kitchenObjectParent;
+
+    private const int MaxIngredientAmount = 5;
+
+    private Dictionary<KitchenObjectType, KitchenObject> _ingredients;
+
+    private void Awake() => 
+        _ingredients = new Dictionary<KitchenObjectType, KitchenObject>(MaxIngredientAmount);
+
+    public bool CanKitchenObjectBeTaken(KitchenObject kitchenObject)
+    {
+        if (kitchenObject == null)
+        {
+            Debug.Log("Object is null!");
+            return false;
+        }
+
+        if (kitchenObject.IsCooked && _ingredients.ContainsKey(kitchenObject.Type) == false)
+            return true;
+
+        return false;
+    }
+
+    public void TakeKitchenObject(KitchenObject kitchenObject)
+    {
+        if (_ingredients.ContainsKey(kitchenObject.Type))
+            return;
+
+        kitchenObject.HasBeenTaken?.Invoke();
+        _ingredients.Add(kitchenObject.Type, kitchenObject);
+        kitchenObject.SetParent(_kitchenObjectParent);
+    }
+}
