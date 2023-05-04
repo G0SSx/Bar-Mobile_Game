b90049c78f8f3970141574b5cded0fa4f7c12147
Ihor <igormochalov1881@gmail.com>
2023-05-05 01:01:02 +0300

    CuttableKitchenObjectConfig class added

---
+++ Assets/_Code/KitchenObjects/CuttableKitchenObjectsConfig.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "CuttableObjectsConfig", menuName = "Configs/CuttableObjectsConfig")]
+public class CuttableKitchenObjectsConfig : ScriptableObject
+{
+    [field:SerializeField] public int TomatoCutPercent { get; private set; } = 34;
+    [field: SerializeField] public int CheeseCutPercent { get; private set; } = 27;
+    [field: SerializeField] public int CabbageCutPercent { get; private set; } = 20;
+}
